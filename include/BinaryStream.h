b810c291209c0143e9d0f1d0e390a0e023b18aad
Jack Noordhuis <daswaggamer@gmail.com>
2019-01-03 15:59:30 +1100

    Add some util classes

---
+++ include/BinaryStream.h
@@ -0,0 +1,350 @@
+//
+// Created by Jack Noordhuis on 23/12/18.
+//
+
+#ifndef CESTIA_BINARYSTREAM_H
+#define CESTIA_BINARYSTREAM_H
+
+#include "../../includes.h"
+
+#include <exception>
+#include <string>
+#include <cstring>
+#include <memory>
+
+class EOFException : public std::exception {
+public:
+    virtual const char *what() const throw() {
+        return "End of stream reached!";
+    }
+};
+
+/**
+ * This class allows reading and writing binary data.
+ */
+class BinaryStream {
+public:
+    bool swapEndian = false;
+
+    virtual ~BinaryStream() {};
+
+    virtual unsigned int read(byte *data, unsigned int size) = 0;
+
+    virtual void write(const byte *data, unsigned int size) = 0;
+
+    inline void readFully(byte *data, unsigned int size) {
+        if(read(data, size) != size) {
+            throw EOFException();
+        }
+    }
+
+    static const int LONG_SIZE = 8;
+    static const int INT_SIZE = 4;
+    static const int SHORT_SIZE = 2;
+    static const int BYTE_SIZE = 1;
+    static const int FLOAT_SIZE = 4;
+    static const int DOUBLE_SIZE = 8;
+
+    static inline unsigned int getStringSize(const std::string &str) {
+        return (unsigned int) (INT_SIZE + str.length());
+    };
+
+    BinaryStream &operator<<(float val) {
+        if(swapEndian) swapBytes((byte*) &val, FLOAT_SIZE);
+        this->write((byte *) &val, FLOAT_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator<<(double val) {
+        if(swapEndian) swapBytes((byte*) &val, DOUBLE_SIZE);
+        this->write((byte *) &val, DOUBLE_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator<<(long long val) {
+        if(swapEndian) swapBytes((byte*) &val, LONG_SIZE);
+        this->write((byte *) &val, LONG_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator<<(unsigned long long val) {
+        *this << (long long) val;
+        return *this;
+    }
+
+    BinaryStream &operator<<(int val) {
+        if(swapEndian) swapBytes((byte*) &val, INT_SIZE);
+        this->write((byte *) &val, INT_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator<<(unsigned int val) {
+        *this << (int) val;
+        return *this;
+    }
+
+    BinaryStream &operator<<(short val) {
+        if(swapEndian) swapBytes((byte*) &val, SHORT_SIZE);
+        this->write((byte *) &val, SHORT_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator<<(unsigned short val) {
+        *this << (short) val;
+        return *this;
+    }
+
+    BinaryStream &operator<<(char val) {
+        this->write((byte *) &val, BYTE_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator<<(unsigned char val) {
+        *this << (char) val;
+        return *this;
+    }
+
+    BinaryStream &operator<<(std::string val) {
+        *this << (int) val.length();
+        this->write((const byte *) val.c_str(), (unsigned int) val.length());
+        return *this;
+    }
+
+    BinaryStream &operator>>(float &val) {
+        this->read((byte *) &val, FLOAT_SIZE);
+        if(swapEndian) swapBytes((byte *) &val, FLOAT_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator>>(double &val) {
+        this->read((byte *) &val, DOUBLE_SIZE);
+        if(swapEndian) swapBytes((byte *) &val, DOUBLE_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator>>(long long &val) {
+        this->read((byte *) &val, LONG_SIZE);
+        if(swapEndian) swapBytes((byte *) &val, LONG_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator>>(unsigned long long &val) {
+        *this >> (long long&) val;
+        return *this;
+    }
+
+    BinaryStream &operator>>(int &val) {
+        this->read((byte *) &val, INT_SIZE);
+        if(swapEndian) swapBytes((byte *) &val, INT_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator>>(unsigned int &val) {
+        *this >> (int&) val;
+        return *this;
+    }
+
+    BinaryStream &operator>>(short &val) {
+        this->read((byte *) &val, SHORT_SIZE);
+        if(swapEndian) swapBytes((byte *) &val, SHORT_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator>>(unsigned short &val) {
+        *this >> (short&) val;
+        return *this;
+    }
+
+    BinaryStream &operator>>(char &val) {
+        this->read((byte *) &val, BYTE_SIZE);
+        return *this;
+    }
+
+    BinaryStream &operator>>(unsigned char &val) {
+        *this >> (char&) val;
+        return *this;
+    }
+
+    BinaryStream &operator>>(std::string &val) {
+        unsigned int strLen;
+        *this >> strLen;
+        byte buf[strLen];
+        this->read(&buf[0], strLen);
+        val = std::string((char *) buf, strLen);
+        return *this;
+    }
+
+    static void swapBytes(byte *array, size_t size) {
+        byte* array2 = new byte[size];
+        memcpy(array2, array, size);
+        for (ssize_t i = size - 1; i >= 0; i--) {
+            array[size - 1 - i] = array2[i];
+        }
+    }
+};
+
+class MemoryBinaryStream : public BinaryStream {
+protected:
+    unsigned int size;
+    byte *data;
+    unsigned int pos = 0;
+    bool allowRealloc = false;
+
+    virtual void resize(unsigned int minimalSize) { }
+public:
+    MemoryBinaryStream(byte *data, unsigned int size);
+
+    /**
+     * This function returns the current used buffer size when writing or the current buffer position when reading.
+     */
+    inline unsigned int getSize() const { return pos; }
+
+    /**
+     * This function returns the current used buffer size when writing or the current buffer position when reading.
+     */
+    inline unsigned int getPos() const { return pos; }
+
+    /**
+     * This function returns the actual size of the buffer.
+     */
+    inline unsigned int getBufferSize() const { return size; }
+
+    /**
+     * This function returns the remaining buffer size (in bytes).
+     * When using a DynamicMemoryBuffer it'll return how many bytes can be still written before a buffer reallocation.
+     */
+    inline unsigned int getRemainingSize() const { return size - pos; }
+
+    unsigned int skip(unsigned int count) {
+        if(pos + count > size) {
+            count = (size - pos);
+        }
+        pos += count;
+        return count;
+    }
+
+    virtual void write(const byte *data, unsigned int size);
+
+    virtual unsigned int read(byte *data, unsigned int size);
+
+};
+
+class DynamicMemoryBinaryStream : public MemoryBinaryStream {
+
+protected:
+    virtual void resize(unsigned int minimalSize);
+
+public:
+    DynamicMemoryBinaryStream(unsigned int size);
+    DynamicMemoryBinaryStream() : DynamicMemoryBinaryStream(1024) {
+        //
+    }
+
+    virtual ~DynamicMemoryBinaryStream() {
+        if(data != nullptr) {
+            delete data;
+        }
+    }
+
+    /**
+     * Pass 'true' if you want to avoid freeing the buffer but are not going to use this object later (trying to read
+     * or write on it will result in a crash!).
+     */
+    byte* getBuffer(bool release);
+
+};
+
+class FileBinaryStream : public BinaryStream {
+
+    int fd = -1;
+    bool closeFdOnDestroy = false;
+public:
+    FileBinaryStream() {}
+    FileBinaryStream(int fd);
+
+    FileBinaryStream(int fd, bool closeFdOnDestroy) : FileBinaryStream(fd) {
+        this->closeFdOnDestroy = closeFdOnDestroy;
+    }
+
+    virtual ~FileBinaryStream();
+
+    void setFileDescriptor(int fd) { this->fd = fd; }
+    inline int getFileDescriptor() { return fd; }
+
+    virtual void write(const byte *data, unsigned int size);
+
+    virtual unsigned int read(byte *data, unsigned int size);
+
+};
+
+class WrapperBinaryStream : public BinaryStream {
+protected:
+    std::unique_ptr<BinaryStream> stream;
+public:
+    WrapperBinaryStream(std::unique_ptr<BinaryStream> stream) : stream(std::move(stream)) {
+        //
+    }
+
+    virtual void write(const byte *data, unsigned int size) {
+        stream->write(data, size);
+    }
+
+    virtual unsigned int read(byte *data, unsigned int size) {
+        return stream->read(data, size);
+    }
+
+};
+
+/*
+class BufferedBinaryStream : public WrapperBinaryStream {
+
+protected:
+    static const unsigned int CHUNK_SIZE = 1024 * 16;
+    byte readData [CHUNK_SIZE];
+    unsigned int readPos = 0;
+    byte writeData [CHUNK_SIZE];
+    unsigned int writePos = 0;
+
+public:
+    BufferedBinaryStream(std::unique_ptr<BinaryStream> stream) : WrapperBinaryStream(std::move(stream)) {
+        //
+    };
+
+    virtual void write(const byte *data, unsigned int size) {
+        if (size >= CHUNK_SIZE) {
+            stream->write(data, size);
+            return;
+        }
+        else if (size >= CHUNK_SIZE - writePos)
+            flush();
+        memcpy(&writeData[writePos], &data[0], size);
+    };
+
+    virtual unsigned int read(byte *data, unsigned int size) {
+        int destOff = 0;
+        if (readPos <= 0) {
+            stream->read(&readData[0], size);
+        }
+
+        if (size < CHUNK_SIZE - readPos) {
+            memcpy(&data[0], &readData[0], size);
+            readPos += size;
+            return;
+        }
+        memcpy(&data[0], &readData[0], CHUNK_SIZE - readPos);
+        destOff += CHUNK_SIZE - readPos;
+        readPos = 0;
+
+        stream->read(&data[destOff], size - destOff);
+    };
+
+    virtual void flush() {
+        if (writePos <= 0)
+            return;
+        stream->write(&writeData[0], writePos);
+        writePos = 0;
+    };
+
+};
+ */
+#endif //CESTIA_BINARYSTREAM_H
---
+++ src/BinaryStream.cpp
@@ -0,0 +1,100 @@
+//
+// Created by Jack Noordhuis on 23/12/18.
+//
+
+#include "BinaryStream.h"
+#include "../Server.h"
+
+#include <unistd.h>
+
+MemoryBinaryStream::MemoryBinaryStream(byte *data, unsigned int size) {
+    this->data = data;
+    this->size = size;
+}
+
+void MemoryBinaryStream::write(const byte *data, unsigned int size) {
+    if(pos + size > this->size) {
+        if(allowRealloc) {
+            resize(pos + size);
+        } else {
+            Server::Log->error("Attempting to write %i bytes to a buffer with only %i bytes remaining!", size, this->size - pos);
+            throw EOFException();
+        }
+    }
+
+    memcpy(&this->data[pos], data, size);
+    pos += size;
+}
+
+unsigned int MemoryBinaryStream::read(byte *data, unsigned int size) {
+    if(pos + size > this->size) {
+        size = (this->size - pos);
+    }
+
+    memcpy(data, &this->data[pos], size);
+    pos += size;
+    return size;
+}
+
+DynamicMemoryBinaryStream::DynamicMemoryBinaryStream(unsigned int size) : MemoryBinaryStream(new byte[size], size) {
+    allowRealloc = true;
+}
+
+void DynamicMemoryBinaryStream::resize(unsigned int minimalSize) {
+    unsigned int newSize = size;
+    while(newSize < minimalSize) {
+        newSize *= 2;
+    }
+    if(newSize == size)
+        return;
+    byte *newBuf = new byte[newSize];
+    memcpy(&newBuf[0], &data[0], size);
+    delete data;
+    data = newBuf;
+    size = newSize;
+}
+
+byte *DynamicMemoryBinaryStream::getBuffer(bool release) {
+    byte *b = data;
+    if(release)
+        data = nullptr;
+    return b;
+}
+
+FileBinaryStream::FileBinaryStream(int fd) {
+    this->fd = fd;
+}
+
+void FileBinaryStream::write(const byte *data, unsigned int size) {
+    int pos = 0;
+    while(true) {
+        ssize_t s = ::write(fd, &data[pos], size - pos);
+        if(s < 0) {
+            throw EOFException();
+        }
+        pos += s;
+        if(pos >= size) {
+            return;
+        }
+    }
+}
+
+unsigned int FileBinaryStream::read(byte *data, unsigned int size) {
+    unsigned int pos = 0;
+    while(true) {
+        ssize_t s = ::read(fd, &data[pos], size - pos);
+        if(s <= 0) {
+            return pos;
+        }
+        pos += s;
+        if(pos >= size) {
+            return pos;
+        }
+    }
+}
+
+FileBinaryStream::~FileBinaryStream() {
+    if(closeFdOnDestroy) {
+        ::close(fd);
+    }
+}
